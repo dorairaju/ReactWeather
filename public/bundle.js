cc4d50e659518a0c58bfc7c82d617bb09e4c22b3
Dorairaju <dorairaj512@gmail.com>
2016-12-22 16:13:54 -0600

    Update middleware for localhost

--- public/bundle.js
+++ public/bundle.js
@@ -28140,19 +28140,21 @@
 	'use strict';
 
 	var React = __webpack_require__(1);
-	// var Examples = React.createClass({
-	//     render: function() {
-	//       return (
-	//         <h3>Examples Component</h3>
-	//       )
-	//     }
-	// });
 
 	var Examples = function Examples(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'Examples Component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'Examples Component'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to examples page!'
+	    )
 	  );
 	};
 
